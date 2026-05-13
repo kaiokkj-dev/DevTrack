0e344fdec36abfca75ce04ad7bb3596554c86ce1
Kaio Henrique <kaiohenriquemalaquias@gmail.com>
2026-05-12 22:37:06 -0300

    orrige websocket supabase na vercel

--- database/db.js
+++ database/db.js
@@ -1,4 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
+const WebSocket = require("ws");
+
+global.WebSocket = WebSocket;
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
